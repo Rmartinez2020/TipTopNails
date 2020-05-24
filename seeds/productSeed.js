4febf92deccbd2960b6704f0b9d625e49aeda0a3
Roger <rmartinez5926.rm@gmail.com>
2020-05-23 22:14:54 -0400

    create a seed for all products

---
+++ seeds/productSeed.js
@@ -0,0 +1,134 @@
+/* === Dependencies === */
+const mongoose = require("mongoose");
+const Product = require("../models/product");
+
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/TipTipNails"
+);
+
+const productSeed = [
+  {
+    name: "Manicure",
+    price: 16,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Manicure/Pedicure",
+    price: 40,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Gel Color Change",
+    price: 20,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Gel Manicure",
+    price: 27,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Pedicure",
+    price: 26,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Deluxe Pedicure",
+    price: 32,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Super Pedicure",
+    price: 47,
+    category: "Manicure Pedicure"
+  },
+  {
+    name: "Acrylic Full Set",
+    price: 27,
+    category: "Nail Care"
+  },
+  {
+    name: "Acrylic Fill",
+    price: 20,
+    category: "Nail Care"
+  },
+  {
+    name: "Pink & White Full Set",
+    price: 42,
+    category: "Nail Care"
+  },
+  {
+    name: "Pink & White Fill",
+    price: 37,
+    category: "Nail Care"
+  },
+  {
+    name: "Pink Fill",
+    price: 27,
+    category: "Nail Care"
+  },
+  {
+    name: "White Tip Full Set",
+    price: 34,
+    category: "Nail Care"
+  },
+  {
+    name: "UV Gel Full Set",
+    price: 47,
+    category: "Nail Care"
+  },
+  {
+    name: "UV Gel Fill",
+    price: 37,
+    category: "Nail Care"
+  },
+  {
+    name: "Full Set",
+    price: 37,
+    category: "Healthy Nails"
+  },
+  {
+    name: "Pink & White Full Set",
+    price: 50,
+    category: "Healthy Nails"
+  },
+  {
+    name: "Pink & White Fill",
+    price: 39,
+    category: "Healthy Nails"
+  },
+  {
+    name: "Pink Fill",
+    price: 27,
+    category: "Healthy Nails"
+  },
+  {
+    name: "Eyebrow",
+    price: 10,
+    category: "Waxing"
+  },
+  {
+    name: "Lips",
+    price: 6,
+    category: "Waxing"
+  },
+  {
+    name: "Chin",
+    price: 7,
+    category: "Waxing"
+  },
+];
+
+
+Product
+  .remove({})
+  .then(() => Product.collection.insertMany(productSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
