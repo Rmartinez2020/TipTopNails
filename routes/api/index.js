a0bb53c679da5d6abf08e18ccf8ce5ce3817d470
Roger <rmartinez5926.rm@gmail.com>
2020-05-23 21:09:03 -0400

    set up api routes index

---
+++ routes/api/index.js
@@ -0,0 +1,12 @@
+/* === Dependencies === */
+const path = require("path");
+const router = require("express").Router();
+
+/* === Routes === */
+
+// For anything else, render the html page
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+module.exports = router;
