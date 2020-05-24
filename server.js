1ef5cce275330893f18aa8ffdfb87a85649ccdfa
Roger <rmartinez5926.rm@gmail.com>
2020-05-23 20:58:36 -0400

    set up server

--- server.js
+++ server.js
@@ -1,24 +1,61 @@
+/* === Dependencies === */
 const express = require("express");
 const path = require("path");
+const mongoose = require('mongoose');
+const routes = require('./routes/index');
+/* === Set the PORT to work with deployment environment === */
 const PORT = process.env.PORT || 3001;
+/* === Call Express as app === */
 const app = express();
 
-// Define middleware here
+/* === Middleware === */
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+/* Serve up static assets */
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, './client/build')));
 }
 
-// Define API routes here
-
+/* === Routing === */
+app.use(routes);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, () => {
+/* === Express 404 error handler === */
+app.use(function (req, res, next) {
+  var err = new Error('404 in Server.js, route Not Found');
+  err.status = 404;
+  next(err);
+});
+/* === Mongoose Connection === */
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/TipTopNails', { useNewUrlParser: true, useUnifiedTopology: true });
+
+/* === Error Handling === */
+
+/* Development error handler will print stacktrace */
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.json({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+/* Production error handler no stacktraces leaked to user */
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: {}
+  });
+});
+
+/* === Telling Express to Listen === */
+app.listen(PORT, function () {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
