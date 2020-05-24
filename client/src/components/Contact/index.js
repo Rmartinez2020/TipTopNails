800ea9968c94f07b5057ece354cc6b484f97d5ed
Roger <rmartinez5926.rm@gmail.com>
2020-05-24 13:21:56 -0400

    create basic contact component

---
+++ client/src/components/Contact/index.js
@@ -0,0 +1,24 @@
+/* === Dependencies === */
+import React from "react";
+
+function Contact() {
+    return (
+        <>
+            <div className="row">
+                <div className="col-sm-4">
+                    Call Us:772-794-2302 or 772-538-8162
+                </div>
+                <div className="col-sm-4">
+                    Address:
+                    772 21st St, Vero Beach, FL 32960
+                </div>
+                <div className="col-sm-4">
+                    Business Hours:
+                    Monday-Saturday 9:00am - 6:30pm
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default Contact;
