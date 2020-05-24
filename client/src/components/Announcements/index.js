c40d7a8868773fead9f69f34b16c2031be5c57f4
Roger <rmartinez5926.rm@gmail.com>
2020-05-23 23:29:42 -0400

    create announcement component

---
+++ client/src/components/Announcements/index.js
@@ -0,0 +1,21 @@
+/* === Dependencies === */
+import React from "react";
+
+/* For now this will be a basic component 
+ Plan for future is to have multiple announcements
+*/
+function Announcements() {
+    return (
+        <>
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">Posted on May 24, 2020</h6>
+                    <p class="card-text">add something later</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Announcements;
