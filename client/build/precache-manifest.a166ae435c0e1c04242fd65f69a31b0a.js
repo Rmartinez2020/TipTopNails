54b82f4be5d682847b4a9b856f977a9ee3e31d70
Roger <rmartinez5926.rm@gmail.com>
2020-05-25 14:28:10 -0400

    set up for deployment

---
+++ client/build/precache-manifest.a166ae435c0e1c04242fd65f69a31b0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6669caace14491cfb0e7",
+    "url": "/static/css/main.56bbf830.chunk.css"
+  },
+  {
+    "revision": "6669caace14491cfb0e7",
+    "url": "/static/js/main.5cba053a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "51c179a90b7fca8054d2",
+    "url": "/static/js/2.f871ad70.chunk.js"
+  },
+  {
+    "revision": "807f41a27ba007a3bbc1e07723accbbf",
+    "url": "/static/media/sun.807f41a2.jpg"
+  },
+  {
+    "revision": "cadc232c2bd5e45ec72c170658538890",
+    "url": "/index.html"
+  }
+];
