5a18e7a35f953dc87a761b0ebd4e6676e6c53cf8
Roger <rmartinez5926.rm@gmail.com>
2020-05-26 17:30:20 -0400

    change the price from numbers to strings in the seed

--- seeds/productSeed.js
+++ seeds/productSeed.js
@@ -10,117 +10,117 @@ mongoose.connect(
 const productSeed = [
   {
     name: "Manicure",
-    price: 16,
+    price: "16",
     category: "Manicure Pedicure"
   },
   {
     name: "Manicure/Pedicure",
-    price: 40,
+    price: "40",
     category: "Manicure Pedicure"
   },
   {
     name: "Gel Color Change",
-    price: 20,
+    price: "20",
     category: "Manicure Pedicure"
   },
   {
     name: "Gel Manicure",
-    price: 27,
+    price: "27",
     category: "Manicure Pedicure"
   },
   {
     name: "Pedicure",
-    price: 26,
+    price: "26",
     category: "Manicure Pedicure"
   },
   {
     name: "Deluxe Pedicure",
-    price: 32,
+    price: "32",
     category: "Manicure Pedicure"
   },
   {
     name: "Gel Pedicure",
-    price: 46,
+    price: "46",
     category: "Manicure Pedicure"
   },
   {
     name: "Super Pedicure",
-    price: 47,
+    price: "47",
     category: "Manicure Pedicure"
   },
   {
     name: "Acrylic Full Set",
-    price: 27,
+    price: "27 & up",
     category: "Nail Care"
   },
   {
     name: "Acrylic Fill",
-    price: 20,
+    price: "20 & up",
     category: "Nail Care"
   },
   {
     name: "Pink & White Full Set",
-    price: 42,
+    price: "50 & up",
     category: "Nail Care"
   },
   {
     name: "Pink & White Fill",
-    price: 37,
+    price: "40",
     category: "Nail Care"
   },
   {
     name: "Pink Fill",
-    price: 27,
+    price: "27",
     category: "Nail Care"
   },
   {
     name: "White Tip Full Set",
-    price: 34,
+    price: "35",
     category: "Nail Care"
   },
   {
     name: "UV Gel Full Set",
-    price: 47,
+    price: "55 & up",
     category: "Nail Care"
   },
   {
     name: "UV Gel Fill",
-    price: 37,
+    price: "37 & up",
     category: "Nail Care"
   },
   {
     name: "Full Set",
-    price: 37,
+    price: "37 & up",
     category: "Healthy Nails"
   },
   {
     name: "Pink & White Full Set",
-    price: 50,
+    price: "50 & up",
     category: "Healthy Nails"
   },
   {
     name: "Pink & White Fill",
-    price: 39,
+    price: "40",
     category: "Healthy Nails"
   },
   {
     name: "Pink Fill",
-    price: 27,
+    price: "30",
     category: "Healthy Nails"
   },
   {
     name: "Eyebrow",
-    price: 10,
+    price: "10",
     category: "Waxing"
   },
   {
     name: "Lips",
-    price: 6,
+    price: "6",
     category: "Waxing"
   },
   {
     name: "Chin",
-    price: 7,
+    price: "7",
     category: "Waxing"
   },
 ];
