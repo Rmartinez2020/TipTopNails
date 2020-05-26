681da2d3b72143ef1c9e00d981ca9f89aa63524a
Roger <rmartinez5926.rm@gmail.com>
2020-05-26 17:48:27 -0400

    fix some html attribute names causing errors in the console

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -4,7 +4,7 @@ import React from "react";
 function Product(props) {
     return (
         <>
-            <div className="card m-auto product" style={{ width: "18rem" }}>
+            <div className="card m-auto product" style={{ width: "20rem" }}>
                 <div className="card-body">
                     <h5 className="card-title float-left bad-text">{props.name}</h5>
                     <p className="card-text float-right bad-text">$<span className="cursive-number bad-text">{props.price}</span></p>
