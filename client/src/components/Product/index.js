11ad02ea57021328ce3319f905c06c322b5506f6
Roger <rmartinez5926.rm@gmail.com>
2020-05-26 17:17:48 -0400

    change font sizes

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -7,11 +7,11 @@ function Product(props) {
             <div className="card m-auto product" style={{ width: "18rem" }}>
                 <div className="card-body">
                     <h5 className="card-title float-left bad-text">{props.name}</h5>
-                    <p className="card-text float-right bad-text">${props.price}</p>
+                    <p className="card-text float-right bad-text">$<span className="cursive-number bad-text">{props.price}</span></p>
                 </div>
             </div>
         </>
-    )
+    );
 };
 
 export default Product;
