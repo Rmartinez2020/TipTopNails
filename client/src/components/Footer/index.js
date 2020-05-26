a9a68fc65ae638243f1082c239bada6720bccf67
Roger <rmartinez5926.rm@gmail.com>
2020-05-26 18:06:53 -0400

    set up footer test

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+/* === Dependencies === */
+import React from "react";
+
+function Footer() {
+    return(
+        <>
+        <footer className="footer">test</footer>
+        </>
+    )
+}
+
+
+export default Footer;
